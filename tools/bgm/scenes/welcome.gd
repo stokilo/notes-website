eff2de5bc1d262fe5e362ce02b203be90289a40a
Slawomir Stec <slawomir.stec.ext@kiongroup.com>
2025-05-21 07:56:28 +0200

    Notes website.

--- tools/bgm/scenes/welcome.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _ready():
-	$StartButton.pressed.connect(_on_start_button_pressed)
-
-func _on_start_button_pressed():
-	# Change to the main game scene
-	get_tree().change_scene_to_file("res://scenes/main.tscn") 
